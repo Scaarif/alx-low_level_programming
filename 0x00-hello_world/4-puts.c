5150b6da5543db465373c08cbd87aa1b539b4af6
scaarifintl <scaarifintl@gmail.com>
2022-06-30 23:31:23 +0300

    include initial short description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char output[] = "\"Programming is like building a multilingual puzzle\n";
+	char output[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(output);
 
