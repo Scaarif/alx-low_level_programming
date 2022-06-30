ea5be63931fbc8a1d5d117ebd385ae344767f6bf
scaarifintl <scaarifintl@gmail.com>
2022-06-30 23:26:32 +0300

    print to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Description: 'print to stdout using puts'
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char output[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(output);
+
+	return (0);
+}
+
