1d699f0f3287c88c436039a04942556eebbc367a
scaarifintl <scaarifintl@gmail.com>
2022-06-30 23:52:08 +0300

    minor fix to formatting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,11 +17,11 @@ int main(void)
 	long int typeLongInt;
 	long long int typeLongLong;
 
-	printf("Size of char: %lu byte(s)\n", sizeof(typeChar));
-	printf("Size of int: %lu byte(s)\n", sizeof(typeInt));
-	printf("Size of float: %lu byte(s)\n", sizeof(typeFloat));
-	printf("Size of long int: %lu byte(s)\n", sizeof(typeLongInt));	
-	printf("Size of long long int: %lu byte(s)\n", sizeof(typeLongLong));
+	printf("Size of char: %zu byte(s)\n", sizeof(typeChar));
+	printf("Size of int: %zu byte(s)\n", sizeof(typeInt));
+	printf("Size of float: %zu byte(s)\n", sizeof(typeFloat));
+	printf("Size of long int: %zu byte(s)\n", sizeof(typeLongInt));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(typeLongLong));
 
 	return (0);
 }
