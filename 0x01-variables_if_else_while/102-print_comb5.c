c4e9fb54bcfcb31d81be20319be1fa077ca98fc0
scaarifintl <scaarifintl@gmail.com>
2022-07-01 23:55:19 +0300

    print all possible two two digit combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,44 +7,40 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i, j, k, l, a, b, c, d;
+	int i, j;
+	int a, b, c, d;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 100; i++)
 	{
-		a = i + '0';
-		for (j = 0; j <=8; j++)
-		{
-			b = j + '0';
-			for (k = i; k <= 9; k++)
-			{
-				c = k + '0';
-				for (l = k + 1; l <= 9; l++)
-				{
-					d = l + '0';
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(c);
-					putchar(d);
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
 
-					if (i == 9 && j == 8 && k == 9 && l == 9)
-					{
-						break;
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
 				}
 			}
 		}
 	}
-
-	putchar('\n');
+	putchar(10);
 
 	return (0);
+
 }
+
