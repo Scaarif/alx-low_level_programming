e5b0b025492b6da748dc623cc2fa9a210f655c52
scaarifintl <scaarifintl@gmail.com>
2022-07-05 23:12:29 +0300

    include stdio.h

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * main - entry point
- *
- * Description: calls the add and _putchar functions as tests
- *
- * Return: always 0
- *
- */
-int main(void)
-{
-	print_to_98(102);
-	print_to_98(72);
-	print_to_98(98);
-	return (0);
-}
 
 /**
  *print_to_98 - print to 98
