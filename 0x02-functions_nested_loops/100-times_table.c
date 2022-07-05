efe0174e73a260ecdcd017c0ae48d296f9e2343a
scaarifintl <scaarifintl@gmail.com>
2022-07-05 23:43:32 +0300

    print times table given an argument n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,49 +9,44 @@
 
 void print_times_table(int k)
 {
-	int i = 0;
+	int x = 0, y, z;
 
-	while (i <= k)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		int m = 0;
-
-		while (m <= k)
+		for (y = 0; y <= n; y++)
 		{
-			int result = i * m;
-
-			if (i == 0)
+			z = x * y;
+			if (z > 99)
 			{
-				_putchar(i + '0');
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			else if (i == 1)
+			else if (z > 9)
 			{
-				_putchar(m + '0');
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
 			}
 			else
+				_putchar(z + '0');
+
+			if (y != n)
 			{
-				if (result > 9 && result < 100)
-				{
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else if (result > 99)
-				{
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else
-				{
-					_putchar(result + '0');
-				}
-			}
-			_putchar(',');
-			if (result <= 9)
+				_putchar(',');
 				_putchar(' ');
-			_putchar(' ');
-			m++;
+			}
 		}
 		_putchar('\n');
-		i++;
+		x++;
 	}
 }
 
