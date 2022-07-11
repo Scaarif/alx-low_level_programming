24f8aebbd0b0ab63207baa31a1c4707bb5d03c01
scaarifintl <scaarifintl@gmail.com>
2022-07-11 20:42:58 +0300

    cater for n <=0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,18 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < (n - 1))
+	if (n > 0)
 	{
-		printf("%d, ", a[i]);
-		i++;
+		while (i < (n - 1))
+		{
+			printf("%d, ", a[i]);
+			i++;
+		}
+		if (i == (n - 1))
+			printf("%d\n", a[i]);
 	}
-	if (i == (n - 1))
-		printf("%d\n", a[i]);
+	else
+	{
+		printf("\n");
+	}
 }
