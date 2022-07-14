b9893d43791977f6b1135f8b6db361a26352e354
scaarifintl <scaarifintl@gmail.com>
2022-07-14 21:30:53 +0300

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+	int first, index;
 
 	for (index = n - 1; index >= n / 2; index--)
 	{
-		tmp = a[n - 1 - index];
+		first = a[n - 1 - index];
 		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+		a[index] = first;
 	}
 }
