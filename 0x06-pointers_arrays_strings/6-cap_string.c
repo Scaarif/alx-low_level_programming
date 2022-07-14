746d1afc70268e18cebb61f70e134ad96228b285
scaarifintl <scaarifintl@gmail.com>
2022-07-14 21:09:57 +0300

    fix rev_array

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,10 @@ char *cap_string(char *s)
 			s[0] = s[0] - 32;
 		}
 		/*check letters folowing the space character*/
-		else if ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		else if ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+		s[i] == ',' || s[i] == ';' || s[i] == '!' || s[i] == '?'
+		|| s[i] == '"' || s[i] == '(' || s[i] == '{' || s[i] == ')'
+		|| s[i] == '}')
 		&& (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 		{
 			s[i + 1] = s[i + 1] - 32;
