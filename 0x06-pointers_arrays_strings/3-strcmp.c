6c6c363b4562441bc89739152ace71453eef0b99
scaarifintl <scaarifintl@gmail.com>
2022-07-14 20:53:22 +0300

    fix _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,22 +9,14 @@
  * s1 is less than, matches, or greater than s2
  * Return: an integer
  */
+ 
 int _strcmp(char *s1, char *s2)
 {
-	int i, res;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-		/*for the first disimilar characters*/
-		if (s1[i] != s2[i])
-		{
-			res = s1[i] - s2[i];
-		}
-		else
-		{
-			res = 0; /* where s1 == s2*/
-		}
+		s1++;
+		s2++;
 	}
-	return (res);
-}
 
+	return (*s1 - *s2);
+}
