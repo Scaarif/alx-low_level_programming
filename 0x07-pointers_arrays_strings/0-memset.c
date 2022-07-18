cec452a843e51690af91a068f438d170c91a9495
scaarifintl <scaarifintl@gmail.com>
2022-07-19 00:23:56 +0300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	i = 0;
-	while (n > 0)
-	{
-		s[i] = b;
-		i++;
-		n--;
-	}
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 	return (s);
 }
