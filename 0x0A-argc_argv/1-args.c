f5749a3b9bcd235e8706f38812dedf3ec27cb8d5
scaarifintl <scaarifintl@gmail.com>
2022-07-22 17:24:45 +0300

    explicitely take care of unused variable

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,14 @@
  * main - prints number of arguments besides program name
  * @argc: arg count
  * @argv: args array
- * Return: always EXIT_SUCCESS
+ * Return: 0 always
  */
 int main(int argc, char **argv)
 {
+	(void) argv;
+	
 	if (argc > 0)
-		printf("%d\n", --argc);
-	exit(EXIT_SUCCESS);
+		printf("%d\n", (argc - 1));
+	return (0);
 }
 
