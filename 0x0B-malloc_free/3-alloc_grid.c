0635933130cdf614f647bdab44cf0a87befeee09
scaarifintl <scaarifintl@gmail.com>
2022-07-28 18:04:16 +0300

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,22 +17,29 @@ int **alloc_grid(int width, int height)
 	if (width + height < 2 || width < 1 || height < 1)
 		return (NULL);
 
+	/*grid is a pointer to pointers (double pointer) - an array of arrays */
+	/*i.e. it is a pointer to 1D arrays (sorta like single rows) in 2D arrays*/
 	grid = malloc(height * sizeof(*grid));
+	/*i.e. total size = size of pointer (array), (int *) * height (no of arrays/ rows) */
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
-
+	}
 	for (i = 0; i < height; i++)
 	{
+		/*now assign memory to individual arrays/rows;*/
 		grid[i] = malloc(width * sizeof(**grid));
+		/* **grid is the value of array elements (type = type of element) */
 		if (grid[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-				free(grid[i]);
-			free(grid);
+				free(grid[i]);/* free row/ array */
+			free(grid);/* free 2D array */
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
+			grid[i][j] = 0;/* initialize the arrays/rows */
 	}
 
 	return (grid);
