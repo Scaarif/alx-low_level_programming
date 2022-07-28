7864739c3c3afb711f61bdf819f0c664643659fa
scaarifintl <scaarifintl@gmail.com>
2022-07-28 18:50:40 +0300

    fix string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,31 +26,25 @@ int get_len(char *s)
  * @s1: string one
  * @s2: string two
  * @n: bytes of s2 to concatenate
- * Description: treates NULL as an empty string
- * Return: pointer to allocated memory of new string
+ * Descristrtion: treates NULL as an emstrty string
+ * Return: strointer to allocated memory of new string
  * :else NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, c, len1, len2, total = 0;
+	unsigned int len1, i, c;
 
-	len1 = (unsigned int) get_len(s1);
-	total += len1;
-	len2 = (unsigned int) get_len(s2);
-	if (len2 <= n)
-		total += len2 + 1;/* account for a terminating character */
-	else
-		total += n + 1;
-	str = malloc(sizeof(char) * total);
-	if (str == NULL)
-		return (NULL);
-	i = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0, c = 0; i < (total - 1); i++)
+
+	len1 = (unsigned int)get_len(s1);
+	str = malloc((len1 + n + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0, c = 0; i < (len1 + n); i++)
 	{
 		if (i < len1)
 			str[i] = s1[i];
@@ -58,6 +52,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			str[i] = s2[c++];
 	}
 	str[i] = '\0';
+
 	return (str);
 }
 
