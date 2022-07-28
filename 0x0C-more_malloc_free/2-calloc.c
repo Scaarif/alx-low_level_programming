ce8f621b5064b179b314401129800dda90564b0e
scaarifintl <scaarifintl@gmail.com>
2022-07-28 20:06:53 +0300

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	c *ptr;
 	/*char *p;*/
 	unsigned int i;
 
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		ptr[i] = 0;
 	return (ptr);
 }
