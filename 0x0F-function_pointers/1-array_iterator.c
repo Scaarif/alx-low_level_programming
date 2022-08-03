70335580a062c7481f4d0c895def68ec7646bfda
scaarifintl <scaarifintl@gmail.com>
2022-08-03 18:08:41 +0300

    provide for null arrays and action

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i = 0;
 
 	/*calls back the fn passed to it with array[i] as arg*/
-	if (size > 0)
+	if (size > 0 && array != NULL && action != NULL)
 	{
 		for (; i < size; i++)
 			action(array[i]);
