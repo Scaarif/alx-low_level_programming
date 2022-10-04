edd33f9437c40666827f7e6a70ef2cfd438e0d82
scaarifintl <scaarifintl@gmail.com>
2022-10-04 23:27:26 +0300

    test1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/Scaarif/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P https://github.com/Scaarif/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=./libmask.so
