f73906411fee48b15a4a0ddbf7adae11fbaa01f7
scaarifintl <scaarifintl@gmail.com>
2022-10-04 23:00:48 +0300

    try own version of make-me-win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/baccrie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/Scaarif/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libmask.so
