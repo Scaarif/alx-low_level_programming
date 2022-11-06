8f6ff098ad7aba0a3a8dfc09697244d639dcfd52
scaarifintl <scaarifintl@gmail.com>
2022-11-06 12:37:28 +0300

    move cocktail_shaker_sort_list to main folder

--- 0x1B-sorting-algorithms_big-O/intro/insertion.c
+++ 0x1B-sorting-algorithms_big-O/intro/insertion.c
@@ -6,6 +6,12 @@
 
 /**
  * insertion_sort - insertion sorts an array
+ * Description: insertion starts with the first element assumed
+ * as the sorted set and the rest of the array/ list unsorted
+ * In each pass, the element [sorted set tail + 1] is compared against
+ * the sorted set, (in a loop) starting with [sorted tail] and that continues
+ * until the element is 'inserted' at its right position (in sorted set)
+ * For all elements past the sorted set's tail (last element) 
  * @arr: the array to sort
  * @n: the size of the array
  * Return: Nothing
