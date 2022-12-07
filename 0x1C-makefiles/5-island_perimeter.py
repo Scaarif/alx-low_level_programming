ccaedb46bbb6b4c9484a0dab1834bdb3ebd81efe
scaarifintl <scaarifintl@gmail.com>
2022-12-07 22:23:18 +0300

    provide for edge cases

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -26,17 +26,49 @@ def island_perimeter(grid):
     # for each col(cell) in row, check if the cell has neighbours:
     # to the left, right, top and bottom and increment perimeter as necessary
     perimeter = 0
+    length, height = len(grid[0]), len(grid)
     for r, row in enumerate(grid):
         for c, cell in enumerate(row):
             if cell == 1:
-                prev, next_ = row[c - 1], row[c + 1]
-                top, bottom = grid[r - 1][c], grid[r + 1][c]
-                if not prev:
-                    perimeter += 1
-                if not next_:
-                    perimeter += 1
-                if not top:
-                    perimeter += 1
-                if not bottom:
-                    perimeter += 1
+                # check that the cell isn't in first/last row
+                # check that the cell isn't in first or last column
+                if c == 0:  # first cell in row
+                    perimeter += 1  # no prev
+                    if not row[c + 1]:  # no next
+                        perimeter += 1
+
+                elif c == length - 1:  # last cell in row
+                    perimeter += 1  # no next_
+
+                    if not row[c - 1]:  # no prev
+                        perimeter += 1
+
+                else:
+                    prev, next_ = row[c - 1], row[c + 1]
+                    if not prev:
+                        perimeter += 1
+
+                    if not next_:
+                        perimeter += 1
+
+                if r == 0:  # a first row cell
+                    perimeter += 1  # no top
+
+                    if not grid[r + 1][c]:  # no bottom
+                        perimeter += 1
+
+                elif r == height - 1:  # a last row cell
+                    perimeter += 1  # no bottom
+
+                    if not grid[r - 1][c]:  # no top
+                        perimeter += 1
+
+                else:
+                    top, bottom = grid[r - 1][c], grid[r + 1][c]
+                    if not top:
+                        perimeter += 1
+
+                    if not bottom:
+                        perimeter += 1
+
     return perimeter
